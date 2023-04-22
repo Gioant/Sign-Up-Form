e76f1a821e32bf53fb0f0317eda79cce0bd2968e
Anthony <gioant1@hotmail.com>
2023-04-22 15:03:24 -0400

    replaced logic for 2nd & 3rd regex scenario
    attempt to fix 2nd & 3rd scenarios by coming regex patterns.

--- validation.js
+++ validation.js
@@ -12,13 +12,12 @@ function validate_FirstName(event) {
     //replace any characters that is not a letter with empty ""
     const newValue = value.replace(/[^a-zA-Z]/g, "");
 
-    // Replace the first letter of the input to uppercase 
-    const upperCase_letter = newValue.replace(/^./, newValue[0].toUpperCase());
-
-    // Replace all uppercase letters after the first letter to lowercase 
-    const finalValue = upperCase_letter.replace(/[A-Z]/g, (char) =>
+    const finalValue = newValue.replace(/^[a-zA-Z]|[\W_]+[a-zA-Z]/g, (char) =>
+        char.toUpperCase()
+    ).replace(/[A-Z]/g, (char) =>
         char.toLowerCase()
     );
+
 
     // Set the value of the input element to the cleaned and formatted value
     event.target.value = finalValue;
